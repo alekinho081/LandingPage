65ba3fe73db7cd5cf59d6c9b88a18bb8852417aa
Alekinho <alexborbajr0@gmail.com>
2025-04-16 17:01:45 -0300

    deletando coisas inutilizadas

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 import './Menu.css';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 import InfoIcon from '@mui/icons-material/Info';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
 import HomeIcon from '@mui/icons-material/Home';
@@ -10,7 +9,6 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
-import { Padding } from "@mui/icons-material";
 
 export const Menu = () => {
     let [light, setLight] = useState(true)
