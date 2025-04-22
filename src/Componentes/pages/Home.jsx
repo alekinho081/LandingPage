40e6be9f37022c8ce1dd7607dca2b640378a6ea4
GABRIEL LINO VASCONCELLOS DOS SANTOS <gabriel_lv_santos@estudante.sesisenai.org.br>
2025-04-22 16:55:57 -0300

    Conclusao da HomePage

--- src/Componentes/pages/Home.jsx
+++ src/Componentes/pages/Home.jsx
@@ -1,13 +1,18 @@
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom"
+import Banner from "../Banner/Banner";
 
 export const Home = () => {
   return (
     <section>
-      <h1>DashBoard TechSolutions</h1>
-      
-      <Link to='/projeto'><BottomNavigationAction/>Saiba Mais</Link>
+      <h1>TechSolutions</h1>
+      <Banner></Banner>
+      <br></br>
+      <br></br>
+      <i style={{fontSize:'20px'}}>Sua próxima revolução automotiva começa aqui.</i>
+      <br></br>
+      <Link to='/projeto'><BottomNavigationAction sx={{marginRight:'-75px'}}/>Saiba Mais</Link>
     </section>
   );
 };
--- src/Componentes/pages/Projeto.jsx
+++ src/Componentes/pages/Projeto.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 
 export const Projeto = () => {
     return (
         <section>
                 <h1>Projeto</h1>
-
         </section>
     )
 
